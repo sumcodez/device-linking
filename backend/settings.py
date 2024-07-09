4731b5f253d733604b77805c8a391ea294433a2f
sumcodez <cloud7suman@gmail.com>
2024-07-09 08:41:10 +0530

    added new requirements.txt file

--- backend/settings.py
+++ backend/settings.py
@@ -1,19 +1,24 @@
-from flask import Flask,request,jsonify
-from flask_cors import CORS,cross_origin
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import JSON
-from flask import Flask,json,Request,jsonify,request
-from flask_cors import CORS
-from sqlalchemy.sql import func
+# from flask import Flask,request,jsonify
+# from flask_cors import CORS,cross_origin
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import JSON
+# from flask import Flask,json,Request,jsonify,request
+# from flask_cors import CORS
+# from sqlalchemy.sql import func
+# from dotenv import load_dotenv
+# import os
 from dotenv import load_dotenv
+from flask import Flask,request,jsonify
+from flask_cors import CORS
 import os
+from flask_sqlalchemy import SQLAlchemy,JSON
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI_VM")
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI_VM")
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = True
 app.config['CORS_HEADERS'] = 'Content-Type'
 
